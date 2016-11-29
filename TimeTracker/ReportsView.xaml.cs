26c01aef2e2ed681019decc3c02226b5d85c8fb8
Michael-James Parsons <mjay.parsons@gmail.com>
2016-11-29 18:39:37 -0500

    Set up reports section with test data.

--- TimeTracker/ReportsView.xaml.cs
+++ TimeTracker/ReportsView.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TimeTracker.Ioc;
+using TimeTracker.ViewModels;
 
 namespace TimeTracker
 {
@@ -23,6 +25,7 @@ namespace TimeTracker
         public ReportsView()
         {
             InitializeComponent();
+            DataContext = IocKernel.Get<ReportsViewModel>();
         }
     }
 }
---
+++ TimeTracker/ViewModels/ReportsViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.ObjectModel;
+using LiveCharts;
+using LiveCharts.Wpf;
+using TimeTracker.Models;
+using TimeTracker.Services;
+
+namespace TimeTracker.ViewModels
+{
+    class ReportsViewModel : BaseViewModel
+    {
+        private MasterViewModel _masterViewModel;
+        private SeriesCollection _weeklyActivityChart;
+
+        public SeriesCollection WeeklyActivityChart
+        {
+            get
+            {
+                return _weeklyActivityChart;
+            }
+
+            set
+            {
+                _weeklyActivityChart = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ReportsViewModel(MasterViewModel masterViewModel)
+        {
+            _masterViewModel = masterViewModel;
+
+            //Test data
+            WeeklyActivityChart = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Values = new ChartValues<double> { 3, 5, 7, 4 }
+                },
+                new LineSeries
+                {
+                    Values = new ChartValues<double> { 1,3,5,2,8,9,10,15 }
+                }
+            };
+        }
+    }
+}
